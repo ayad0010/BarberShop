d82dd0a13b95220d886b444c6efd51baebb62971
Anusha <ayad0010@student.monash.edu>
2025-06-28 23:53:08 +1000

    Created css files for main page

--- barber-shop/src/pages/home.js
+++ barber-shop/src/pages/home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './home.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to The Barber Shop!</h1>
+        <div className="HomeScreenText">
+            <h3>STAY SHARP, LOOK GOOD</h3>
+            <h1>NYC'S FAVOURITE <br />BARBER SHOP</h1>
+            <h3>DIAGON ALLEY, NYC. APPOINTMENT:999 000 8888</h3>
         </div>
 
     );
