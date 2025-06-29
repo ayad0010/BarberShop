21cf11004dc9c4775a89136d9a825a1f43bb373e
Anusha <ayad0010@student.monash.edu>
2025-06-29 23:27:37 +1000

    added button fucnction and developed home page

--- barber-shop/src/pages/home.js
+++ barber-shop/src/pages/home.js
@@ -8,14 +8,47 @@ const Home = () => {
     }
 
     return (
-        <div className="HomeScreenText">
+        <div>
+            <div className="HomeScreenText">
             <h3>STAY SHARP, LOOK GOOD</h3>
             <h1>NYC'S FAVOURITE <br />BARBER SHOP</h1>
             <h3>DIAGON ALLEY, NYC. APPOINTMENT:999 000 8888</h3>
+            <br />
             <button onClick={bookingClick}>
-                Book Appointment
+                BOOK APPOINTMENT
             </button>
+            </div>
+
+            <div className="Welcome">
+                <h2>Welcome to The Barber Shop!</h2>
+            </div>
+
+            <div className="Services">
+                <h2>Services</h2>
+            </div>
+
+            <div className="Opening-Hours">
+                <h2>Opeming Hours</h2>
+            </div>
+
+            <div className="Reviews">
+                <h2>Client Reviews</h2>
+            </div>
+
+            <div className="Gallery">
+                <h2>Gallery</h2>
+            </div>
+
+            <div className="News">
+                <h2>News</h2>
+            </div>
+
+            <div className="Footer">
+                <h2>Footer</h2>
+            </div>
+
         </div>
+        
     );
 };
 
