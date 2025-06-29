3e20c3f7c0ff89a49e55261d94b9503b8964ffb8
Anusha <ayad0010@student.monash.edu>
2025-06-29 22:43:11 +1000

    button for booking created

--- barber-shop/src/pages/home.js
+++ barber-shop/src/pages/home.js
@@ -3,11 +3,18 @@ import './home.css';
 import barberBg from '../Images/barber-background.jpg';
 import Navbar from '../Navbar';
 const Home = () => {
+    const bookingClick = () => {
+        alert('Booking click!');
+    }
+
     return (
         <div className="HomeScreenText">
             <h3>STAY SHARP, LOOK GOOD</h3>
             <h1>NYC'S FAVOURITE <br />BARBER SHOP</h1>
             <h3>DIAGON ALLEY, NYC. APPOINTMENT:999 000 8888</h3>
+            <button onClick={bookingClick}>
+                Book Appointment
+            </button>
         </div>
     );
 };
